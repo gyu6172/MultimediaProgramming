b6d14e3fb3339cf4997e726b7910a25a3296834a
gyu6172 <gyu6172@gmail.com>
2023-04-18 19:39:50 +0900

    0418-중간고사 연습(이미지 하프토닝)

--- 멀티미디어프로그래밍/0411-패턴법.cpp
+++ 멀티미디어프로그래밍/0411-패턴법.cpp
@@ -2,28 +2,29 @@
 int main() {
 
 
-	IplImage* src = cvLoadImage("C:\\tmp\\lena.png");
+	IplImage* src = cvLoadImage("C:\\tmp\\세종대학교.jpg");
 	CvSize size = cvGetSize(src);
 	IplImage* dst = cvCreateImage(size, 8, 3);
-	IplImage* blur = cvCreateImage(size, 8, 3);
 
 	int pat[9] = { 1,8,0,6,2,5,7,3,4 };
 	//int pat[4] = {2,1,3,0};
 	int numPat = 9;
 
 	cvSet(dst, cvScalar(0, 0, 0));
-	cvSmooth(src, blur, CV_BLUR, 5);
 
 	for (int y = 0; y < size.height - 2; y += 3) {
 		for (int x = 0; x < size.width - 2; x += 3) {
 			CvScalar f = cvGet2D(src, y, x);
 			for (int k = 0; k < 3; k++) {
 
-				int bri = f.val[k];
+				//int bri = f.val[k];
+				//
+				//float div = 255.0f / (numPat+1);
+				//
+				//int lev = bri / div;	//level개의 흰색 점을 칠해야 한다.
 
-				float div = 255.0f / (numPat+1);
+				int lev = (f.val[k]/255.0f)*(numPat+1);
 
-				int lev = bri / div;	//level개의 흰색 점을 칠해야 한다.
 				//int(0~1) : 0
 				//int(1~2) : 1
 				//int(2~3) : 2
@@ -33,7 +34,7 @@ int main() {
 				//lev = (f.val[k]*(numPat+1))/255
 				
 				//딱 numPat+1이 되면 오버플로우. 그 현상을 방지하기 위한 if문
-				if (lev > numPat) lev = numPat;
+				if (lev == numPat+1) lev = numPat;
 
 				//lev개만큼 점을 칠해야한다.
 				for (int i = 0; i < lev; i++) {
@@ -43,17 +44,14 @@ int main() {
 					g.val[k] = 255;
 
 					cvSet2D(dst, y2, x2, g);
-					cvShowImage("dst", dst);
-					cvWaitKey();
+					
 				}
 			}
 		}
 	}
 
-
+	cvShowImage("dst", dst);
 	cvShowImage("src", src);
-
-	cvShowImage("blur", blur);
 	cvWaitKey();
 
 	return 0;
--- 멀티미디어프로그래밍/이미지 하프토닝 연습.cpp
+++ 멀티미디어프로그래밍/이미지 하프토닝 연습.cpp
@@ -8,11 +8,11 @@ float getDiff(CvScalar f, CvScalar g) {
 }
 
 void thresholdingImage(IplImage *src, CvScalar threshArr[], int size);
-void patterningImage(IplImage *src, int patternArr[], int size);
+void patterningImage(IplImage *src, int patternArr[], int arrSize);
 void ditheringImage(IplImage *src, CvScalar threshArr[], int size);
 
 int main() {
-	IplImage *src = cvLoadImage("C:\\tmp\\lena.png");
+	IplImage *src = cvLoadImage("C:\\tmp\\세종대학교.jpg");
 	CvSize size = cvGetSize(src);
 
 	IplImage *ditheringImg = cvCreateImage(size, 8, 3);
@@ -67,36 +67,72 @@ void thresholdingImage(IplImage* src, CvScalar threshArr[], int size)
 	cvShowImage("thresh", threshImg);
 }
 
-void patterningImage(IplImage* src, int patternArr[], int size)
+void patterningImage(IplImage* src, int patternArr[], int arrSize)
 {
-	int height = cvGetSize(src).height;
-	int width = cvGetSize(src).width;
 
-	IplImage* patternImg = cvCreateImage(cvGetSize(src), 8, 3);
+	CvSize size = cvGetSize(src);
+	IplImage* patternImg = cvCreateImage(size, 8, 3);
+
+	int height = size.height;
+	int width = size.width;
+
 	cvSet(patternImg, cvScalar(0,0,0));
 
-	for (int y = 0; y < height-2; y+=3) {
-		for (int x = 0; x < width-2; x+=3) {
+	for (int y = 0; y < height-2; y += 3) {
+		for (int x = 0; x < width-2; x += 3) {
+
 			CvScalar f = cvGet2D(src,y,x);
 
 			for (int k = 0; k < 3; k++) {
-				//0 <= paintCnt <=10
-				int paintCnt = (f.val[k]/255.0f)*(size+1);
-				if (paintCnt > 9) {
-					paintCnt = 9;
+				//0 <= paintCnt <= 10
+
+				int paintCnt = (f.val[k]/255.0f)*(arrSize+1);
+
+				if (paintCnt > arrSize) {
+					paintCnt = arrSize;
 				}
 				for (int idx = 0; idx < paintCnt; idx++) {
-					int nx = x+patternArr[idx]%3;
-					int ny = x+patternArr[idx]/3;
+					int nx = x + patternArr[idx] % 3;
+					int ny = y + patternArr[idx] / 3;
 
 					CvScalar g = cvGet2D(patternImg,ny,nx);
 					g.val[k] = 255;
 
 					cvSet2D(patternImg, ny, nx, g);
+					
 				}
 			}
 		}
 	}
 
 	cvShowImage("pattern", patternImg);
+}
+
+void ditheringImage(IplImage* src, CvScalar threshArr[], int size)
+{
+	int height = cvGetSize(src).height;
+	int width = cvGetSize(src).width;
+
+	IplImage* ditherImg = cvCreateImage(cvGetSize(src), 8, 3);
+
+	for (int y = 0; y < height; y++) {
+		for (int x = 0; x < width; x++) {
+			CvScalar f = cvGet2D(src, y, x);
+			CvScalar g;
+
+			float minDiff = FLT_MAX;
+			int minDiffIdx = 0;
+			for (int idx = 0; idx < size; idx++) {
+				if (minDiff > getDiff(f, threshArr[idx])) {
+					minDiff = getDiff(f, threshArr[idx]);
+					minDiffIdx = idx;
+				}
+			}
+
+			g = threshArr[minDiffIdx];
+
+			cvSet2D(ditherImg, y, x, g);
+		}
+	}
+	cvShowImage("thresh", ditherImg);
 }
