6bb989b49bbfde8ada59c1d928b29a42ae62c7b6
gyu6172 <gyu6172@gmail.com>
2023-05-04 18:03:38 +0900

    0504-과제4 구조체 정의

--- 멀티미디어프로그래밍/과제4.cpp
+++ 멀티미디어프로그래밍/과제4.cpp
@@ -3,8 +3,23 @@ typedef struct Stroke {
 	int radius;
 	CvScalar color;
 	CvPoint startPoint;
-	CvScalar route[];
+	CvPoint route[];
 }Stroke;
+typedef struct Circle {
+	int radius;
+	CvScalar color;
+	CvPoint point;
+}Circle;
+typedef struct Grid{
+	int colsCnt;	//격자판의 가로 개수
+	int rowsCnt;	//격자판의 세로 개수
+	int width;		//격자 하나의 가로 길이(너비)
+	int height;		//격자 하나의 세로 길이(높이)
+};
+
+void drawCircle(IplImage* img, Circle circle) {
+	cvCircle(img, circle.point, circle.radius, circle.color, -1);
+}
 
 int main() {
 	IplImage* srcImg = cvLoadImage("C:\\tmp\\lena.png");
@@ -14,8 +29,17 @@ int main() {
 
 	cvSmooth(srcImg, refImg, CV_GAUSSIAN, 11);
 
+	Grid jitterdGrid;
+	jitterdGrid.width = 64;
+	jitterdGrid.height = 64;
+	jitterdGrid.colsCnt = (imgSize.width) / (jitterdGrid.width);
+	jitterdGrid.rowsCnt = (imgSize.height) / (jitterdGrid.height);
+
 	cvShowImage("src",srcImg);
 	cvShowImage("ref",refImg);
+
+
+
 	cvWaitKey();
 	
 }
