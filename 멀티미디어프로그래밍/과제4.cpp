f355629f35f18f1e86df67138a3d8165c986ab43
gyu6172 <gyu6172@gmail.com>
2023-05-06 10:53:11 +0900

    0506-과제4 그리드마다 원 그림

--- 멀티미디어프로그래밍/과제4.cpp
+++ 멀티미디어프로그래밍/과제4.cpp
@@ -27,6 +27,14 @@ float getDifference(CvScalar f, CvScalar g) {
 	}
 	return sum;
 }
+void shuffleArr(Circle circleArr[], int size) {
+	for (int i = 0; i < size; i++) {
+		int ran = rand()%size;
+		Circle tmp = circleArr[i];
+		circleArr[i] = circleArr[ran];
+		circleArr[ran] = tmp;
+	}
+}
 
 int main() {
 	IplImage* srcImg = cvLoadImage("C:\\tmp\\lena.png");
@@ -41,44 +49,64 @@ int main() {
 	int h = imgSize.height;
 
 	Grid jitterdGrid;
-	jitterdGrid.width = 32;
-	jitterdGrid.height = 32;
+	jitterdGrid.width = 40;
+	jitterdGrid.height = 40;
 	jitterdGrid.colsCnt = (imgSize.width) / (jitterdGrid.width);
 	jitterdGrid.rowsCnt = (imgSize.height) / (jitterdGrid.height);
 
-	for (int y = 0; y < h; y+=jitterdGrid.height) {
-		for (int x = 0; x < w; x+=jitterdGrid.width) {
-			printf("x:%d, y:%d\n",x,y);
-			float max_diff = 0;
-			CvPoint max_diff_pos = {0,0};
-			CvScalar max_diff_color = cvScalar(255,255,255);
+	cvShowImage("ref", refImg);
 
-			for (int v = y; v < y + jitterdGrid.height; v++) {
-				for (int u = x; u < x + jitterdGrid.width; u++) {
-					if (u > w - 1 || v > h - 1)	continue;
+	int r=32;
+	while (r > 2) {
+		//Circle *circleArr = (Circle*)malloc(sizeof(Circle)*(jitterdGrid.colsCnt*jitterdGrid.rowsCnt));
+		Circle circleArr[20000];
+		int circleCnt = 0;
 
-					CvScalar refColor = cvGet2D(refImg, v, u);
-					CvScalar canvasColor = cvGet2D(canvas, v, u);
+		for (int y = 0; y < h; y += jitterdGrid.height) {
+			for (int x = 0; x < w; x += jitterdGrid.width) {
+				float max_diff = 0;
+				CvPoint max_diff_pos = { 0,0 };
+				CvScalar max_diff_color = cvScalar(255, 255, 255);
 
-					float diff = getDifference(refColor, canvasColor);
-					if (diff > max_diff) {
-						max_diff = diff;
-						max_diff_pos.x = u;
-						max_diff_pos.y = v;
-						max_diff_color = refColor;
-						printf("diff:%.2f, x:%d, y:%d\n", max_diff, u, v);
+				for (int v = y; v < y + jitterdGrid.height; v++) {
+					for (int u = x; u < x + jitterdGrid.width; u++) {
+						if (u > w - 1 || v > h - 1)	continue;
+
+						CvScalar refColor = cvGet2D(refImg, v, u);
+						CvScalar canvasColor = cvGet2D(canvas, v, u);
+
+						float diff = getDifference(refColor, canvasColor);
+						if (diff > max_diff) {
+							max_diff = diff;
+							max_diff_pos.x = u;
+							max_diff_pos.y = v;
+							max_diff_color = refColor;
+						}
+
 					}
-					
 				}
-			}
 
-			Circle c = {jitterdGrid.height, max_diff_color, max_diff_pos};
-			drawCircle(canvas, c);
+				Circle c = { r, max_diff_color, max_diff_pos };
+				circleArr[circleCnt++] = c;
+			}
 		}
+
+		printf("circleCnt : %d\n", circleCnt);
+		shuffleArr(circleArr, circleCnt);
+		for (int i = 0; i < circleCnt; i++) {
+			drawCircle(canvas, circleArr[i]);
+		}
+
+		r/=2;
+		jitterdGrid.width /= 2;
+		jitterdGrid.height /= 2;
+		jitterdGrid.colsCnt = (imgSize.width) / (jitterdGrid.width);
+		jitterdGrid.rowsCnt = (imgSize.height) / (jitterdGrid.height);
+		//free(circleArr);
+		cvShowImage("canvas", canvas);
+		cvWaitKey(1000);
 	}
 
-	cvShowImage("ref", refImg);
-	cvShowImage("canvas", canvas);
 	cvWaitKey();
 	
 }
