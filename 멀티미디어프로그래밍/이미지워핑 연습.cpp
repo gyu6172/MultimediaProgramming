2a722fb73848151a75b951c3baab6600fcec4a9f
gyu6172 <gyu6172@gmail.com>
2023-06-09 20:01:14 +0900

    0609-이미지워핑 연습 - 역변형

--- 멀티미디어프로그래밍/이미지워핑 연습.cpp
+++ 멀티미디어프로그래밍/이미지워핑 연습.cpp
@@ -51,6 +51,34 @@ void setMultiplyMatrix(float M[][3], float A[][3], float B[][3]) {
 	}
 }
 
+void setMultiply(float M[][3], int* x, int* y, int* w, int ex, int ey, int ew) {
+	*x = M[0][0]*ex+M[0][1]*ey+M[0][2]*ew;
+	*y = M[1][0]*ex+M[1][1]*ey+M[1][2]*ew;
+	*w = M[2][0]*ex+M[2][1]*ey+M[2][2]*ew;
+}
+
+void setInverseMatrix(float matrix[3][3], float inverse[3][3]) {
+	float determinant = 0;
+
+	// 행렬의 행렬식 계산
+	for (int i = 0; i < 3; i++) {
+		determinant += (matrix[0][i] * (matrix[1][(i + 1) % 3] * matrix[2][(i + 2) % 3] - matrix[1][(i + 2) % 3] * matrix[2][(i + 1) % 3]));
+	}
+
+	// 행렬식이 0이면 역행렬이 존재하지 않음
+	if (determinant == 0) {
+		printf("역행렬이 존재하지 않습니다.\n");
+		return;
+	}
+
+	// 역행렬 계산
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			inverse[i][j] = ((matrix[(j + 1) % 3][(i + 1) % 3] * matrix[(j + 2) % 3][(i + 2) % 3]) - (matrix[(j + 1) % 3][(i + 2) % 3] * matrix[(j + 2) % 3][(i + 1) % 3])) / determinant;
+		}
+	}
+}
+
 int main() {
 	IplImage* src = cvLoadImage("C:\\tmp\\lena.png");
 	CvSize size = cvGetSize(src);
@@ -76,20 +104,45 @@ int main() {
 
 	float M[3][3];
 	setMultiplyMatrix(M, T, M1);
+	
+	//정변형
+	//for (int y1 = 0; y1 < h; y1++) {
+	//	for (int x1 = 0; x1 < w; x1++) {
+	//		int w1 = 1;
 
-	for (int y1 = 0; y1 < h; y1++) {
-		for (int x1 = 0; x1 < w; x1++) {
-			int w1 = 1;
+	//		int x2 = M[0][0]*x1 + M[0][1]*y1 + M[0][2]*w1;
+	//		int y2 = M[1][0]*x1 + M[1][1]*y1 + M[1][2]*w1;
+	//		int w2 = M[2][0]*x1 + M[2][1]*y1 + M[2][2]*w1;
 
-			int x2 = M[0][0]*x1 + M[0][1]*y1 + M[0][2]*w1;
-			int y2 = M[1][0]*x1 + M[1][1]*y1 + M[1][2]*w1;
-			int w2 = M[2][0]*x1 + M[2][1]*y1 + M[2][2]*w1;
+	//		x2 /= w2;
+	//		y2 /= w2;
 
-			x2 /= w2;
-			y2 /= w2;
+	//		if(x2<0 || x2>w-1) continue;
+	//		if(y2<0 || y2>h-1) continue;
 
-			if(x2<0 || x2>w-1) continue;
-			if(y2<0 || y2>h-1) continue;
+	//		CvScalar f = cvGet2D(src, y1, x1);
+	//		cvSet2D(dst, y2, x2, f);
+	//	}
+	//}
+
+	float IM[3][3];
+	setInverseMatrix(M, IM);
+
+	//역변형
+	for (int y2 = 0; y2 < h; y2++) {
+		for (int x2 = 0; x2 < w; x2++) {
+			int w2 = 1.0f;
+
+			int x1;
+			int y1;
+			int w1;
+			setMultiply(IM, &x1, &y1, &w1, x2, y2, w2);
+
+			x1 /= w1;
+			y1 /= w1;
+
+			if(x1<0 || x1>w-1) continue;
+			if(y1<0 || y1>h-1) continue;
 
 			CvScalar f = cvGet2D(src, y1, x1);
 			cvSet2D(dst, y2, x2, f);
